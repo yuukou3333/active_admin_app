7ae94708c5655df8ec3294cfa686575e981d009a
yuukou3333 <kuusou3333@gmail.com>
2020-01-27 01:35:35 +0900

    active_adminを使ったアプリ

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ActiveAdminApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+    config.i18n.default_locale = :ja
+  end
+end
